7ce7a99699b4eacd7eca9d300361801804b466d1
Tyler Winters <maxdavin1984@gmail.com>
2019-10-02 22:58:06 -0700

    started and worked on hw

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# bamazon
+# Fantasy Bamazon
+While using your Amazon app a mysterious incident pulled you into a world filled with fantasy and magic.
+In order to prepare you should equip yourself with some new gear. Check out the Fantasy Bamazon shop for your adventuring needs.
+
+# Intro
+The user interacts with a database built in mysql through node.js
+
+## Beginning steps
+Once the user starts the application they're greeted with a table of the items in the faux shop and the choice to "buy something" or "exit"
+![welcome](./screenshots/welcome-screen.jpg)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,58 +47,11 @@ function start() {
     })
 }
 
-// function to handle posting new items up for auction
 function buyItem() {
-    connection.query("SELECT * FROM products", function(err, results) {
-        if (err) throw err;
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
-      {
-        name: "item",
-        type: "input",
-        choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_name);
-            }
-            return choiceArray;
-      },
-      message: "What item do you want? (use the item ID#)"
-    },
-      {
-        name: "item",
-        type: "input",
-        message: "How many?"
-      }      
-
-    ])
-    .then(function(answer) {
-        
-    //   // when finished prompting, insert a new item into the db with that info
-    //   connection.query(
-    //     "INSERT INTO auctions SET ?",
-    //     {
-    //       item_name: answer.item,
-    //       category: answer.category,
-    //       starting_bid: answer.startingBid || 0,
-    //       highest_bid: answer.startingBid || 0
-    //     },
-    //     function(err) {
-    //       if (err) throw err;
-    //       console.log("Your auction was created successfully!");
-    //       // re-prompt the user for if they want to bid or post
-    //       start();
-    //     }
-    //   );
-    // });
-}
-
-function bidAuction() {
-  // query the database for all items being auctioned
-  connection.query("SELECT * FROM auctions", function(err, results) {
+  // query the database 
+  connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
+    // 
     inquirer
       .prompt([
         {
@@ -107,35 +60,35 @@ function bidAuction() {
           choices: function() {
             var choiceArray = [];
             for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_name);
+              choiceArray.push(results[i].id);
             }
             return choiceArray;
           },
-          message: "What auction would you like to place a bid in?"
+          message: "What item do you want to buy? (use the item ID#)"
         },
         {
-          name: "bid",
+          name: "stock",
           type: "input",
-          message: "How much would you like to bid?"
+          message: "How many would you like to buy?"
         }
       ])
       .then(function(answer) {
         // get the information of the chosen item
         var chosenItem;
         for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
+          if (results[i].id === answer.choice) {
             chosenItem = results[i];
           }
         }
 
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
+        // determine if stock is enough
+        if (chosenItem.amount_stock < parseInt(answer.stock)) {
           // bid was high enough, so update db, let the user know, and start over
           connection.query(
-            "UPDATE auctions SET ? WHERE ?",
+            "UPDATE products SET ? WHERE ?",
             [
               {
-                highest_bid: answer.bid
+                amount_stock: answer.stock
               },
               {
                 id: chosenItem.id
@@ -149,10 +102,11 @@ function bidAuction() {
           );
         }
         else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
+          // not enough stock
+          console.log("Not enough in stock");
           start();
         }
       });
   });
 }
+
